a6753c5f79dad16055b855ed1f92d54fa79c3b0f
kswhite15 <58196468+kswhite15@users.noreply.github.com>
2021-02-11 18:07:14 -0500

    ?

---
+++ public/labs/lab_3/test.js
@@ -0,0 +1,16 @@
+/* Put your javascript in here */
+
+const images = document.querySelector('.images')
+let gallery = ["onigiri_1.png", "onigiri_2.png", "onigiri_3.png", 
+"onigiri_4.png", "roll_1", "roll_2.png", "roll_3.png"]
+
+let displayed = 3
+
+function forward(){
+    
+
+}
+
+function backward(){
+
+}
